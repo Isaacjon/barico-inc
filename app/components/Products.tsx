e2a57201470f7b14fc008764110e616673cc8c95
isaidakbar <smahmudxojaev@gmail.com>
2025-05-16 22:10:11 +0500

    modifies: adds

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -5,63 +5,57 @@ const products = [
     name: 'Barium Carbonate',
     image: '/product-images/bari_car.png',
     description: '',
+    styleCss: 'row-span-2',
+    clickOur: () => window.open('/product/barium', '_self'),
+    imgCss: ''
   },
   {
     name: 'Скоро...',
     image: '/product-images/bari_sul.png', // Replace with actual path or imported image
     description: '',
+    styleCss: '',
+    clickOur: () => null,
+    imgCss: 'blur-sm'
   },
   {
     name: 'Скоро...',
     image: '/product-images/bari_clor.png',
     description: '',
+    styleCss: 'col-start-2 row-start-2',
+    clickOur: () => null,
+    imgCss: 'blur-sm'
+
   }
 ];
 
- export const Products = () => {
+export const Products = () => {
   return (
     <div className=" bg-white p-8">
       <h2 className="text-3xl font-bold text-center mb-8">Что мы предлагаем ?</h2>
-      <div className="sm:flex sm:flex-wrap md:grid md:grid-cols-3 md:grid-rows-2 md:gap-8 md:w-2/5 mx-auto">
+      <div className="h-[500px] w-3/5 mx-auto grid grid-cols-2 grid-rows-2 gap-12">
         {products.map((product, idx) => (
-          idx === 0 ? (<>
-          <div
-          key={idx + product.image}
-          className="flex flex-col md:col-span-2 md:row-span-2 items-center bg-white shadow hover:shadow-lg transition-shadow cursor-pointer"
-          onClick={() => window.open('/protocol.pdf', '_blank')}
-        >
-          <img
-            src={product.image}
-            alt={product.name}
-            className="w-full h-full object-cover mb-4"
-          />
-          <h3 className="text-lg font-semibold text-gray-800 text-center">
-            {product.name}
-          </h3>
-          {product.description && (
-            <p className="text-sm text-gray-500 text-center">
-              {product.description}
-            </p>
-          )}
-        </div>
-          </>) : <div
-          key={idx}
-          className="flex flex-col md:col-start-3 items-center bg-white shadow hover:shadow-lg transition-shadow my-10"
-        >
-          <img
-            src={product.image}
-            alt={product.name}
-            className="w-full h-full object-cover mb-4 rounded"
-          />
-          <h3 className="text-lg font-semibold text-gray-800 text-center">
-            {product.name}
-          </h3>
-          {product.description && (
-            <p className="text-sm text-gray-500 text-center">
-              {product.description}
-            </p>
-          )}
-        </div>
+          <>
+            <div
+              key={idx + product.image}
+              className={`
+                flex 
+                flex-col
+                items-center
+              bg-white shadow hover:shadow-2xl 
+                transition-shadow cursor-pointer
+          ` + product.styleCss}
+              onClick={product.clickOur}
+            >
+              <img
+                src={product.image}
+                alt={product.name}
+                className={`w-full h-full object-cover ` + product.imgCss}
+              />
+              <h3 className="text-lg font-semibold text-gray-800 text-center">
+                {product.name}
+              </h3>
+            </div>
+          </>
         ))}
       </div>
     </div>
